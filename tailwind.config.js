468e5c9c6bb36554f0b067038567df6dda6e5cb9
idkbit <ggwpanda@gmail.com>
2022-04-12 12:09:25 +0600

    added frontendmentor files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryOrange: 'hsl(26, 100%, 55%)',
+        primaryPaleOrange: 'hsl(25, 100%, 94%)',
+        neutralVDB: 'hsl(220, 13%, 13%)',
+        neutralDGB: 'hsl(219, 9%, 45%)',
+        neutralGB: 'hsl(220, 14%, 75%)',
+        white: 'hsl(0, 0%, 100%)',
+        lightboxBG: 'hsl(0, 0%, 0%)',
+      },
+    },
   },
   plugins: [],
 };
