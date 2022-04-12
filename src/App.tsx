84e3385a991e90c44adea649e78ee0edb8d7d232
idkbit <ggwpanda@gmail.com>
2022-04-12 15:16:06 +0600

    hover&focus styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Header } from './components';
 
 function App() {
   return (
-    <div className='bg-white lg:max-w-7xl m-auto'>
+    <div className='bg-white xl:max-w-7xl lg:max-w-4xl sm:max-w-2xl m-auto'>
       <Header />
     </div>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,18 +6,20 @@ const categories = ['Collections', 'Men', 'Women', 'About', 'Contact'];
 export const Header = () => {
   return (
     <header className='border-b-[1px] border-b-neutralGB flex justify-between pt-4 items-baseline'>
-      <div className='flex items-baseline'>
-        <h1 className='text-4xl font-bold text-neutralVDB lg:mr-12 lowercase'>
+      <div className='flex items-baseline pb-8 lg:pb-0'>
+        <h1 className='text-4xl font-bold text-neutralVDB lg:mr-12 lowercase tracking-tight'>
           sneakers
         </h1>
-        <nav>
+        <nav className='hidden lg:block'>
           <ul className='text-neutralGB flex gap-8 font-bold'>
             {categories.map((category) => (
               <li
                 key={category}
-                className='hover:text-neutralVDB hover:border-b-primaryOrange border-b-4 pb-8 border-b-transparent'
+                className='hover:text-neutralVDB hover:border-b-primaryOrange border-b-4 pb-8 border-b-transparent focus-within:text-primaryOrange focus-within:border-b-primaryOrange'
               >
-                <a href='#'>{category}</a>
+                <a className='focus:outline-none' href='#'>
+                  {category}
+                </a>
               </li>
             ))}
           </ul>
